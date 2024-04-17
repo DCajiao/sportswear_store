7556ef8ddca931183f107c1c4295028b9ebb9d15
David Alejandro Cajiao Lazt <123507601+DCajiao@users.noreply.github.com>
2024-04-16 23:28:43 -0500

    Querys arreglados

--- Scripts/query.sql
+++ Scripts/query.sql
@@ -14,19 +14,19 @@ COUNT(fecha_compra) = (SELECT MAX(fechas) FROM (SELECT fecha_compra, COUNT(fecha
 FROM compra GROUP BY fecha_compra) AS maximo);
 
 -- ¿Cuál es la sección en la que se venden más productos?
-SELECT seccion, COUNT(seccion) FROM producto AS p GROUP BY seccion HAVING 
-COUNT(seccion) = (SELECT MAX(suma_secciones) FROM (SELECT seccion, COUNT(seccion) AS suma_secciones
-FROM producto AS p GROUP BY seccion) AS maximo);
+SELECT Seccion_Articulo, COUNT(Seccion_Articulo) FROM producto AS p GROUP BY Seccion_Articulo HAVING 
+COUNT(Seccion_Articulo) = (SELECT MAX(suma_secciones) FROM (SELECT Seccion_Articulo, COUNT(Seccion_Articulo) AS suma_secciones
+FROM producto AS p GROUP BY Seccion_Articulo) AS maximo);
 
--- ¿Cuales productos estan en el paquete 3?
-SELECT p.Tipo_Articulo FROM producto AS p JOIN almacena ON Identificacion = Producto_identificacion
-WHERE paquete_ID_paquete = 3;
+-- ¿Cuales productos estan en el paquete 3? (Descartada ya que se tuvo que modificar la base de datos para que funcionara correctamente)
+-- SELECT p.Tipo_Articulo FROM producto AS p JOIN almacena ON Identificacion = Producto_identificacion
+-- WHERE paquete_ID_paquete = 3;
 
 -- ¿Cuantos productos de tipo articulo tiene la tienda?
-SELECT COUNT(Tipo) AS numArticulos FROM producto WHERE Tipo = 'Articulo';
+SELECT COUNT(Tipo_Producto) AS numArticulos FROM producto WHERE Tipo_Producto = 'Articulo';
 
 -- ¿Cuál es el producto con el precio más alto?
-SELECT Tipo, Tipo_Articulo, Precio 
+SELECT Tipo_Producto, Tipo_Articulo, Precio 
 FROM producto 
 WHERE Precio = (SELECT MAX(Precio) FROM producto);
 
@@ -41,19 +41,21 @@ FROM persona
 GROUP BY Genero;
 
 -- ¿Cuál es el cliente que ha realizado la compra más cara en términos de costo total?
-SELECT p.Nombre, c.Costo_total 
-FROM persona AS p 
-JOIN compra AS c ON p.Identificacion = c.Cuenta_Usuario 
-WHERE c.Costo_total = (SELECT MAX(Costo_total) FROM compra);
+SELECT C.Cuenta_Usuario, C.Costo_total AS Compra_mas_cara
+FROM Compra AS C
+WHERE C.Costo_total = (
+        SELECT MAX(Costo_total)
+        FROM Compra
+);
 
 -- CONSULTA ANIDADA -->
 -- ¿Cuál es la sección con el precio promedio más alto por artículo vendido?
-SELECT p.Seccion, AVG(p.Precio) AS PrecioPromedio 
+SELECT p.Seccion_Articulo, AVG(p.Precio) AS PrecioPromedio 
 FROM producto AS p 
 JOIN contiene AS c ON p.Identificacion = c.Producto_Identificacion 
-GROUP BY p.Seccion 
+GROUP BY p.Seccion_Articulo 
 HAVING AVG(p.Precio) = (SELECT MAX(PrecioPromedio)
                         FROM (SELECT AVG(p.Precio) AS PrecioPromedio 
                                 FROM producto AS p 
                                 JOIN contiene AS c ON p.Identificacion = c.Producto_Identificacion 
-                                GROUP BY p.Seccion) AS PreciosPorSeccion);
+                                GROUP BY p.Seccion_Articulo) AS PreciosPorSeccion);
