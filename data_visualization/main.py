b9a8f8a6c39cb8a40407cdac2ebab1968f6151c6
David Alejandro Cajiao Lazt <123507601+DCajiao@users.noreply.github.com>
2024-04-20 15:16:33 -0500

    First data visualization commit

---
+++ data_visualization/main.py
@@ -0,0 +1,5 @@
+# API con Flask para 
+
+# 1. actualizar las gráficas en tiempo real.
+# 2. cargarlas en la página web.
+# 3. Retornarlas a POSTMAN.
---
+++ data_visualization/plotter.py
@@ -0,0 +1,5 @@
+# Creador de gráficas
+
+# 1. Recibe los dataframes de las VIEWS
+# 2. Crea las gráficas con Matplotlib
+# 3. Almacena las gráficas en una ruta específica (…/Fronted/assets/images/*)
