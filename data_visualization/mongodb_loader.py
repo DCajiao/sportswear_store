b7616654fe77a795df6a912c4c8ee1ec25345263
David Alejandro Cajiao Lazt <123507601+DCajiao@users.noreply.github.com>
2024-04-22 22:22:17 -0500

    mongodb_loader.py created

---
+++ data_visualization/mongodb_loader.py
@@ -0,0 +1,5 @@
+# Conexión y Extracción de datos de la base en MongoDB.
+
+# 1. Conexión a la base de datos localhost en MongoDB.
+# 2. Consulta a las VIEWS precargadas en la base de datos.
+# 3. Almacenar el reponse de las views en dataframes.
--- data_visualization/sql_loader.py
+++
@@ -1,5 +0,0 @@
-# Conexión y Extracción de datos de la base de datos SQL
-
-# 1. Conexión a la base de datos localhost en SQL.
-# 2. Consulta a las VIEWS precargadas en la base de datos.
-# 3. Almacenar el reponse de las views en dataframes.
